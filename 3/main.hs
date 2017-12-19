7c9943886ca4d4f788437afd866be7ffada6931e
Mateo Gianolio <gianoliomateo@gmail.com>
2017-12-19 19:25:02 +0100

    3a solved

--- 3/main.hs
+++ 3/main.hs
@@ -1,13 +1,31 @@
 parse :: String -> Int
 parse x = read x :: Int
 
+isqrt :: Int -> Float
+isqrt = sqrt . fromIntegral
+
+distance :: (Int, Int) -> Int
+distance (x, y) = abs(x) + abs(y)
+
+spiral :: Int -> (Int, Int)
+spiral n
+  | n >= m - v      = (k - (m - n), -k)
+  | n >= m - 2 * v  = (-k, -k + (m - v - n))
+  | n >= m - 3 * v  = (-k + (m - 2 * v - n), k)
+  | otherwise       = (k, k - (m - 3 * v - n))
+  where
+    k = ceiling (((isqrt n) - 1)  / 2)
+    t = 2 * k + 1
+    v = t - 1
+    m = t^2
+
 disp :: Show a => a -> IO ()
 disp = putStrLn . show
 
 main = do
   input <- getContents
 
-  let x = parse input
-  -- abs(x) + abs(y) = x
+  let n = parse input
+  let a1 = distance $ spiral n
 
-  disp x
+  disp a1
