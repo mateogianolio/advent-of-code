28446b1cc0de2fcdc2e150a91b50661229d255c6
Mateo Gianolio <gianoliomateo@gmail.com>
2017-12-20 18:03:59 +0100

    5 solved, ableit slow as hell

---
+++ 5/main.hs
@@ -0,0 +1,42 @@
+import qualified Data.Vector as V
+
+parse :: String -> V.Vector Int
+parse = V.fromList . map read . lines
+
+escape :: Int -> V.Vector Int -> Bool
+escape n xs = n < 0 || n + 1 > length xs
+
+jump :: Int -> Int -> V.Vector Int -> Int
+jump n step xs
+  | escape n xs  = step
+  | x == 0       = jump n (step + 1) (V.update xs (V.fromList [(n, value)]))
+  | otherwise    = jump (n + x) (step + 1) (V.update xs (V.fromList [(n, value)]))
+  where
+    x = xs V.! n
+    value = (x + 1)
+
+jumpSlow :: Int -> Int -> V.Vector Int -> Int
+jumpSlow n step xs
+  | escape n xs  = step
+  | x == 0       = jumpSlow n (step + 1) (V.update xs (V.fromList [(n, value)]))
+  | otherwise    = jumpSlow (n + x) (step + 1) (V.update xs (V.fromList [(n, value)]))
+  where
+    x = xs V.! n
+    value
+      | x >= 3 = (x - 1)
+      | otherwise = (x + 1)
+
+disp :: Show a => a -> IO ()
+disp = putStrLn . show
+
+main = do
+  input <- getContents
+
+  let list = parse input
+  let a1 = jump 0 0 list
+
+  disp a1
+
+  let a2 = jumpSlow 0 0 list
+
+  disp a2
